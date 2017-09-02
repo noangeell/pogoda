4a50c13563ad7507a710b0f6e83473708b62bd52
noangeell <noangeell@gmail.com>
2017-09-03 01:43:50 +0300

    Add files via upload

---
+++ pogoda.py
@@ -0,0 +1,15 @@
+import pyowm
+
+city = input ('Какой город вас интересует?: ')
+
+owm = pyowm.OWM ('89b83544ba0c2ed1c4be507026bbb6d6')
+observation = owm.weather_at_place(city)
+w = observation.get_weather()
+temperatura = w.get_temperature('celsius') ['temp'] 
+veter = w.get_wind()
+vlash = w.get_humidity()
+detalstat = w.get_detailed_status()
+print ("В городе " + city + "," + " " + "сейчас температура: " + str (temperatura) + " " + "по Цельсию." )
+print ( "Направление и скорость ветра" + " " + str (veter) )
+print ("Влажность в городе" + " " + city + " " + str (vlash) )
+print ( detalstat + "\n" + "На сегодня все, спасибо за пользование программы!)")
